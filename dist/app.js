68bb36a8b14344faef87cdede3ec97883d635667
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-13 22:58:45 +0200

    add first task

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,8 @@
-console.log('asdaasdads111');
+const inputOne = document.querySelector('#input1');
+const inputTwo = document.querySelector('#input2');
+const btn = document.querySelector('#buttonAdd');
+btn.addEventListener('click', () => {
+    let one = Number(inputOne.value);
+    let two = Number(inputTwo.value);
+    console.log(one + two);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('asdaasdads111');
+const inputOne: HTMLInputElement = document.querySelector('#input1');
+const inputTwo: HTMLInputElement = document.querySelector('#input2');
+const btn = document.querySelector('#buttonAdd');
+
+btn.addEventListener('click', () => {
+  let one: number = Number(inputOne.value);
+  let two: number = Number(inputTwo.value);
+  console.log(one + two);
+});
