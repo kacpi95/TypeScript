d4ab7a938d88efa60f8fb8dac1b1843574a91036
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-14 11:50:51 +0200

    add: structure index.html

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('#button');
-const calculate = (originalPrice, hasDiscount) => {
-    return hasDiscount ? originalPrice * 0.8 : originalPrice;
-};
-btn.addEventListener('click', () => {
-    const originalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get('discount') === 'true';
-    console.log(calculate(originalPrice, hasDiscount));
-});
