a7b46cb58b1ca325974dfbc418ab7606bc7c3107
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-14 12:25:07 +0200

    add: function addTask

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,23 @@
 const tasksContainerElement = document.querySelector('.tasks');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('#name');
 const tasks = ['Wyrzucić śmeici', 'Pójść na siłkę', 'Nakarmić koty'];
 const render = () => {
+    tasksContainerElement.innerHTML = '';
     tasks.forEach((task) => {
         const taskElement = document.createElement('li');
         taskElement.innerHTML = task;
         tasksContainerElement.appendChild(taskElement);
     });
 };
+const addTask = (e) => {
+    e.preventDefault();
+    const text = input.value;
+    tasks.push(text);
+    input.value = '';
+};
+btn.addEventListener('click', (e) => {
+    addTask(e);
+    render();
+});
 render();
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 const tasksContainerElement: HTMLElement = document.querySelector('.tasks');
+const btn: HTMLButtonElement = document.querySelector('.btn');
+const input: HTMLInputElement = document.querySelector('#name');
 
 const tasks: string[] = ['Wyrzucić śmeici', 'Pójść na siłkę', 'Nakarmić koty'];
 
 const render = () => {
+  tasksContainerElement.innerHTML = '';
   tasks.forEach((task) => {
     const taskElement: HTMLElement = document.createElement('li');
     taskElement.innerHTML = task;
@@ -10,4 +13,15 @@ const render = () => {
   });
 };
 
-render()
+const addTask = (e: Event) => {
+  e.preventDefault();
+  const text = input.value;
+  tasks.push(text);
+  input.value = '';
+};
+
+btn.addEventListener('click', (e: Event) => {
+  addTask(e);
+  render();
+});
+render();
